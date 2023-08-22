8fe4d49f12909ba3a87c01892c22aed0e5b5af65
RizkyZaki <ifsry416@gmail.com>
2023-08-22 08:28:45 +0700

    :sparkels: Testing

---
+++ test/class.test.ts
@@ -0,0 +1,9 @@
+describe("Class", () => {
+  it("Should can create class", () => {
+    class Customer {}
+    class Order {}
+
+    const customer: Customer = new Customer();
+    const order = new Order();
+  });
+});
